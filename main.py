2b812512c79d3f24a23fc20aa0956eb985d40199
icy <i@icys.top>
2022-05-04 15:10:00 +0800

    web

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import flask
+from PIL import Image
 from flask import request
 import os
 import cv2
+import cv2 as cv
 import json
+import random
+from deeplab import DeeplabV3
 
 app = flask.Flask(__name__)
+deeplab = DeeplabV3()
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-
-    return '123'
+    f = request.files['file_data']
+    num = random.randint(1, 1000)
+    dir = './tmp/'
+    end = f.filename.split('.')[-1]
+    img = os.path.join(dir, str(num) + end)
+    f.save(img)
+    image = Image.open(img)
+    r_image = deeplab.detect_image(image)
+    r_image.save('tmp.jpg')
+    src = cv.imread('tmp.jpg')
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    ret, binary = cv2.threshold(gray, 177, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    max = 0
+    final = None
+    for i in contours:
+        if len(i) >= max:
+            final = i
+    final = final.tolist()
+    final = json.dumps(final)
+    return final
 
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return flask.render_template('index.html')
 
 def main():
     app.run()
